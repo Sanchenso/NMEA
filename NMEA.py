a3f3af0ac5567861acd5bba9e6fbf1708884c3f3
alexandr <sanekspbproff@yandex.ru>
2024-09-23 19:47:20 +0300

    upgrade parser NMEA

--- NMEA.py
+++ NMEA.py
@@ -9,6 +9,7 @@ import matplotlib.dates as mdates
 from matplotlib.dates import SecondLocator
 
 nameFile = sys.argv[1]  # for example 'test.ubx'
+
 #systemName = sys.argv[2]  # for example 'GPS'
 #IDsystem = sys.argv[3]  # for example 'L1'
 #print(nameFile, systemName, IDsystem)
